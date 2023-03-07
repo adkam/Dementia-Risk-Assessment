a1c68a4a75a4b09964438adf06008951575aca8d
Ashish <adkamath@umich.edu>
2023-03-06 20:49:36 -0500

    Adds initial backend with flask and gitignore.

---
+++ backend/src/main.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+# creating the Flask application
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/assessment', methods=['POST'])
+def create_assessment():
+    props = request.get_json()
+    true = True
+    return jsonify(true), 201
