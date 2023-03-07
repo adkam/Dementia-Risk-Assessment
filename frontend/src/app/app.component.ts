d3d4ba38f53e394cbda72e1dcf46c41526c37ca0
Ashish <adkamath@umich.edu>
2023-03-07 00:35:52 -0500

    Adds initial form.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { AssessmentApi } from './assessment.api';
 
 @Component({
@@ -6,12 +7,25 @@ import { AssessmentApi } from './assessment.api';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dementia Risk Assessment';
+  formData: any;
 
   constructor(private api: AssessmentApi) {}
 
-  createAssessment() {
-    this.api.createAssessment();
+  ngOnInit(): void {
+    this.formData = new FormGroup({
+      age: new FormControl(''),
+      sex: new FormControl(''),
+      ceradScore: new FormControl(''),
+      gad1: new FormControl(''),
+      gad2: new FormControl(''),
+      gfap: new FormControl(''),
+    });
+  }
+
+  createAssessment(formData: any) {
+    console.log(formData);
+    // this.api.createAssessment();
   }
 }
