debb3d17db2826f17aab0d48de92733369711c79
Ashish <adkamath@umich.edu>
2023-04-04 17:52:49 -0400

    CORS attempt again.

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,6 +8,13 @@ api = Api(app)
 CORS(app, resources={r"/assessment": {"origins": "http://localhost:4200"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 api.add_resource(Assessment, '/assessment')
 
 if __name__ == '__main__':
